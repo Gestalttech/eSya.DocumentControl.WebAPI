1832e1ab86cd12ed2d0b47db04ba644219da4111
AbdulR <abdulr@gestalttechindia.in>
2024-04-27 12:32:11 +0530

    Removed unused model

--- eSya.DocumentControl.DL/eSya.DocumentControl.DL/obj/Debug/net6.0/eSya.DocumentControl.DL.AssemblyInfo.cs
+++ eSya.DocumentControl.DL/eSya.DocumentControl.DL/obj/Debug/net6.0/eSya.DocumentControl.DL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eSya.DocumentControl.DL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb04fbfa2b227d2fbf177b6eb3d5cfd41152ce20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f49c1b3bac16f55767fa5b7a3cdaa731acd9923")]
 [assembly: System.Reflection.AssemblyProductAttribute("eSya.DocumentControl.DL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eSya.DocumentControl.DL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
